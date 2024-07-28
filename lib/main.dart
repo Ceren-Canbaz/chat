0a0c0dcb93061a2f1df5dc9ecab04a010bdb46e4
Ceren-Canbaz <ceren_canbaz@hotmail.com>
2024-07-28 18:37:04 +0300

    fix(ios): package version incompatibles fixed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
-import 'package:chat/core/themes/dark_theme.dart';
 import 'package:chat/core/themes/light_theme.dart';
 import 'package:chat/features/auth/presentation/features/auth/presentation/pages/auth_page.dart';
-import 'package:chat/features/auth/presentation/features/auth/presentation/pages/login_page.dart';
+import 'package:chat/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
 }
 
